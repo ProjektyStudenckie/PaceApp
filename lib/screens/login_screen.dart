335cc36ccb65e64340bf715bce3c4720f57028bf
Jakub Sosna <jakub.sosna@icloud.com>
2021-04-07 22:12:05 +0200

    Introduce back button and password confirmation

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -22,73 +22,91 @@ class _LoginScreenState extends State<LoginScreen> {
     String password;
 
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: SafeArea(
+        child: Stack(
           children: [
-            Flexible(
-              child: Hero(
-                tag: 'logo',
-                child: Container(
-                  height: 200.0,
-                  child: Image.asset('images/logo.png'),
-                ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 0.0, horizontal: 15.0),
+              child: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
             ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: kTextFieldDecoration,
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              obscureText: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Enter your password'),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              color: Colors.lightBlueAccent,
-              text: 'Log in',
-              onPressed: () async {
-                if (email == null || password == null) {
-                  ToastUtils.showCustomToast(context, "Fill all the fields!");
-                  return;
-                }
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Flexible(
+                    child: Hero(
+                      tag: 'logo',
+                      child: Container(
+                        height: 200.0,
+                        child: Image.asset('images/logo.png'),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 48.0,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    decoration: kTextFieldDecoration,
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                        hintText: 'Enter your password'),
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  RoundedButton(
+                    color: Colors.lightBlueAccent,
+                    text: 'Log in',
+                    onPressed: () async {
+                      if (email == null || password == null) {
+                        ToastUtils.showCustomToast(
+                            context, "Fill all the fields!");
+                        return;
+                      }
 
-                if (!isEmailValid(email)) {
-                  ToastUtils.showCustomToast(context, "Email is invalid!");
-                  return;
-                }
+                      if (!isEmailValid(email)) {
+                        ToastUtils.showCustomToast(
+                            context, "Email is invalid!");
+                        return;
+                      }
 
-                try {
-                  final user = await _auth.signInWithEmailAndPassword(
-                      email: email, password: password);
+                      try {
+                        final user = await _auth.signInWithEmailAndPassword(
+                            email: email, password: password);
 
-                  print(user);
-                  if (user != null) {
-                    Navigator.pushNamed(context, MainScreen.id);
-                  }
-                } catch (e) {
-                  print(e);
-                }
-              },
+                        print(user);
+                        if (user != null) {
+                          Navigator.pushNamed(context, MainScreen.id);
+                        }
+                      } catch (e) {
+                        print(e);
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
           ],
         ),
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -21,80 +21,115 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
     final _auth = FirebaseAuth.instance;
     String email;
     String password;
+    String password_confirm;
 
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: SafeArea(
+        child: Stack(
           children: [
-            Flexible(
-              child: Hero(
-                tag: 'logo',
-                child: Container(
-                  height: 200.0,
-                  child: Image.asset('images/logo.png'),
-                ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 0.0, horizontal: 15.0),
+              child: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
             ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: kTextFieldDecoration,
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              obscureText: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your password',
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Flexible(
+                    child: Hero(
+                      tag: 'logo',
+                      child: Container(
+                        height: 200.0,
+                        child: Image.asset('images/logo.png'),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 48.0,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    decoration: kTextFieldDecoration,
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your password',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      password_confirm = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Re-Enter your password',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  RoundedButton(
+                    color: Colors.blueAccent,
+                    text: 'Register',
+                    onPressed: () async {
+                      if (email == null || password == null) {
+                        ToastUtils.showCustomToast(context, "Fill all the fields!");
+                        return;
+                      }
+      
+                      if (!isEmailValid(email)) {
+                        ToastUtils.showCustomToast(context, "Email is invalid!");
+                        return;
+                      }
+      
+                      if (password_confirm != password) {
+                        ToastUtils.showCustomToast(context, "Passwords are not equal!");
+                        return;
+                      }
+      
+                      if (!isPasswordValid(password)) {
+                        ToastUtils.showCustomToast(context, "Password is invalid!");
+                        return;
+                      }
+      
+                      try {
+                        final newUser = await _auth.createUserWithEmailAndPassword(
+                            email: email, password: password);
+                        if (newUser != null) {
+                          print('registration completed');
+                          Navigator.pushNamed(context, MainScreen.id);
+                        }
+                      } catch (e) {
+                        print(e);
+                      }
+                    },
+                  ),
+                ],
               ),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              color: Colors.blueAccent,
-              text: 'Register',
-              onPressed: () async {
-                if (email == null || password == null) {
-                  ToastUtils.showCustomToast(context, "Fill all the fields!");
-                  return;
-                }
-
-                if (!isEmailValid(email)) {
-                  ToastUtils.showCustomToast(context, "Email is invalid!");
-                  return;
-                }
-
-                if (!isPasswordValid(password)) {
-                  ToastUtils.showCustomToast(context, "Password is invalid!");
-                  return;
-                }
-
-                try {
-                  final newUser = await _auth.createUserWithEmailAndPassword(
-                      email: email, password: password);
-                  if (newUser != null) {
-                    print('registration completed');
-                    Navigator.pushNamed(context, MainScreen.id);
-                  }
-                } catch (e) {
-                  print(e);
-                }
-              },
             ),
           ],
         ),
